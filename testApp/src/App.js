31c88699fb951f42c25975edee2e44488373a51d
FelSiq <felipe.siqueira@usp.br>
2018-05-12 18:11:59 -0300

    Added some flexible lists testing.

--- testApp/src/App.js
+++ testApp/src/App.js
@@ -5,19 +5,49 @@ import Comp from './Comp/Comp';
 import './Comp/Comp.css'
 
 class App extends Component {
-
 	state = {
 		persons : [
-			{ name: 'Felipe', age: 22 }
+			{ id: '4388', name: 'Felipe', age: 22 },
+			{ id: '9246', name: 'Larissa', age: 23 },
+			{ id: '4105', name: 'Rose', age: 24 },
+			{ id: '1592', name: 'Ricardo', age: 25 },
+			{ id: '4067', name: 'Priscilla', age: 26 }
 		],
-		showInformation: false
+		showInformation: true,
+		buttonText: 'Hide'
 	}
 
 	toggleNameHandler = () => {
 		const doesShow = this.state.showInformation;
-		this.setState({showInformation: !doesShow});
+		this.setState({
+			buttonText: this.state.showInformation ? 'Show' : 'Hide',
+			showInformation: !doesShow, 
+		});
+	}
+	
+	deletePersonHandler = (personId) => {
+		const newData = [...this.state.persons];
+		newData.splice(personId, 1);
+		this.setState({persons: newData});
 	}
 
+	changeNameHandler = (event, personId) => {
+		const targetPersonIndex = this.state.persons.findIndex(inst => {
+			return inst.id === personId;
+		});
+
+		const personTargetUpdated = {
+			...this.state.persons[targetPersonIndex]
+		};
+
+		personTargetUpdated.name = event.target.value;
+
+		const allPersonsUpdatedData = [...this.state.persons];
+		allPersonsUpdatedData[targetPersonIndex] = personTargetUpdated
+
+		this.setState( { persons: allPersonsUpdatedData } );
+	}
+	
 	render() {
 		const style = {
 			backgroundColor: 'white',
@@ -30,19 +60,23 @@ class App extends Component {
 		let information = null;
 		if (this.state.showInformation) {
 			information = (<div>
-				<Comp 
-				name={this.state.persons[0].name} 
-				age={this.state.persons[0].age} 
-				changed={this.nameChangedHandler}/>
+				{this.state.persons.map((personData, index) => {
+					return 	<Comp
+						buttonDelete = { this.deletePersonHandler.bind(this, index) }
+						key = { personData.id }
+						name = { personData.name }
+						changeBox = { (event) => this.changeNameHandler(event, personData.id) }
+						age = { personData.age } />})}
 			</div>)
 		}
 
 		return (
 			<div className="App">
-				<h1> This is a test App!</h1>
+				<h1> React test app </h1>
 				<button 
 					style={style}
-					onClick={this.toggleNameHandler}>Hide information</button>		 
+					onClick={this.toggleNameHandler}>
+					{this.state.buttonText} information</button>		 
 				{ information }
 			</div>
 		 );
--- testApp/src/Comp/Comp.js
+++ testApp/src/Comp/Comp.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
 import './Comp.css';
 
 const comp = (props) => {
 	return (
 		<div className='Comp'>
-			<p onClick={props.click}>Hello! I'm {props.name} and i'm {props.age}!</p>
+			<p>Hello! I'm {props.name} and i'm {props.age}!</p>
 			<p>{props.children}</p>
-			<input type='text' onChange={props.changed} />
+			<p>
+			Type a new name:
+			<input type='text' value={props.name} onChange={props.changeBox}/>
+			<button onClick={props.buttonDelete}> Delete Person </ button>
+			</p>
 		</div>
 	);
 }
